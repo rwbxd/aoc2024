c8af031e687fa1cee8cd4e2d268c5083990fcea4
rwbxd <will.bryan99@gmail.com>
2024-12-06 16:39:31 -0600

    finish day 5

--- day5.py
+++ day5.py
@@ -30,98 +30,59 @@ test_input = """47|53
 61,13,29
 97,13,75,29,47"""
 
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.depends_on = set()
-    def depend(self, val, seen=None, path=None):
-        if not seen: seen = set()
-        if self.val in seen: return False
-        else: seen.add(self.val)
-
-        if not path: path = []
-        if self.val == val: return path + [self.val]
-
-        return any(dep.val == val for dep in self.depends_on)
-
 def part1(text):
-    node_dict = {}
+    dep_dict = {}
     rules, pages = text.split("\n\n")
     for rule in rules.split("\n"):
         l,r = rule.split("|")
-        l_node = node_dict.setdefault(l, Node(l))
-        r_node = node_dict.setdefault(r, Node(r))
-        r_node.depends_on.add(l_node)
+        dep_dict.setdefault(r, set()).add(l)
 
-    total_valid, total_invalid = 0,0
     result = 0
     for line in pages.split("\n"):
         p = line.split(",")
         valid = True
         for i in range(len(p)):
-            if not valid: break
-            if p[i] not in node_dict:
+            if p[i] not in dep_dict:
                 continue
             for j in range(i+1, len(p)):
-                if node_dict[p[i]].depend(p[j]):
+                if p[j] in dep_dict[p[i]]:
                     valid = False
                     break
             if not valid: break
         if valid:
-            total_valid += 1
             result += int(p[len(p) // 2])
-        else: total_invalid += 1
-    print(total_valid, total_invalid)
     return result
 
 assert part1(test_input) == 143
 print("Part 1:", part1(real_input))
 
+def part2_helper(page_list, dep_dict):
+    invalid_tuple = ()
+    for i in range(len(page_list)):
+        if page_list[i] not in dep_dict: continue
+        for j in range(i+1, len(page_list)):
+            if page_list[j] in dep_dict[page_list[i]]:
+                invalid_tuple = (i,j)
+                break
+        if len(invalid_tuple): break
+    return invalid_tuple
+
 def part2(text):
-    node_dict = {}
+    dep_dict = {}
     rules, pages = text.split("\n\n")
     for rule in rules.split("\n"):
         l,r = rule.split("|")
-        l_node = node_dict.setdefault(l, Node(l))
-        r_node = node_dict.setdefault(r, Node(r))
-        r_node.depends_on.add(l_node)
-
+        dep_dict.setdefault(r, set()).add(l)
     result = 0
     for line in pages.split("\n"):
         p = line.split(",")
-        valid = True
-        highest_valid = 0
-        for i in range(len(p)):
-            if not valid: break
-            if p[i] not in node_dict:
-                continue
-            for j in range(i+1, len(p)):
-                if node_dict[p[i]].depend(p[j]):
-                    valid = False
-                    break
-            if valid: highest_valid = i + 1
-        if valid: continue
-        else:
-            perms = list(itertools.permutations(p[highest_valid:]))
-            perm_index = -1
-            for perm in perms:
-                perm_index += 1
-                print(str(perm_index) + "/" + str(len(perms)))
-                perm = p[:highest_valid] + list(perm)
-                valid = True
-                for i in range(len(p)):
-                    if not valid: break
-                    if perm[i] not in node_dict:
-                        continue
-                    for j in range(i+1, len(perm)):
-                        if node_dict[perm[i]].depend(perm[j]):
-                            valid = False
-                            break
-                if valid:
-                    result += int(perm[len(perm)//2])
-                    break
-
-
+        invalid_tuple = part2_helper(p, dep_dict)
+        if not len(invalid_tuple): continue
+        while len(invalid_tuple):
+            x, y = invalid_tuple
+            p[x], p[y] = p[y], p[x]
+            invalid_tuple = part2_helper(p, dep_dict)
+        result += int(p[len(p)//2])
     return result
 
 assert part2(test_input) == 123
